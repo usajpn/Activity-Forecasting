45e43e487e199756250440fb86ada7030199cfb2
Shinnosuke Usami <susami@andrew.cmu.edu>
2019-12-25 17:50:42 -0500

    add setup data script

---
+++ setup_data.sh
@@ -0,0 +1,6 @@
+
+wget http://www.cs.cmu.edu/~kkitani/forecasting/code/ioc_demo.zip
+wget http://www.cs.cmu.edu/~kkitani/forecasting/code/oc_demo.zip
+
+unzip ioc_demo.zip -d ./env/virat
+unzip oc_demo.zip -d ./env/virat
